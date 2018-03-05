bf55dc14f2dbd1014e13c65ff967e531ec8ac210
Bacra Woo <bacra.woo@gmail.com>
2018-03-05 19:06:47 +0800

    Add: clearWordsAfterOutput options for i18nc-po

--- tasks/i18nc-po.js
+++ tasks/i18nc-po.js
@@ -5,16 +5,23 @@ module.exports = function(grunt)
 	grunt.registerMultiTask('i18nc-po', 'Create i18nc po files.', function()
 	{
 		var outputDir = this.data.output;
-		var options = this.options();
+		var options = this.options(
+			{
+				clearWordsAfterOutput: true
+			});
+
 		if (!outputDir) return;
 		var translateWords = grunt.i18nc && grunt.i18nc.translateWords;
-
 		if (!translateWords) return;
 
 		var done = this.async();
 		i18nc.util.mulitResult2POFiles(translateWords, outputDir, options)
 			.then(function()
 			{
+				if (options.clearWordsAfterOutput)
+				{
+					grunt.i18nc.translateWords = {};
+				}
 				done();
 			},
 			done);
